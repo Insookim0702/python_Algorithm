1bfe943c3c118a60244b6667f7de34c734ac980c
insookim0702 <insookim0702@gmail.com>
2020-09-29 18:46:57 +0900

    2020.09.28(월)

---
+++ 책/DFSBFS/DFS.py
@@ -0,0 +1,13 @@
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+
+#graph = [[], [2, 3, 8], [1, 7], [1, 4, 5], [3, 5], [3, 4], [7], [2, 6, 8], [1, 7]]
+ice = [0,0,1,0,1,0,1,0,1]
+graph = [[], [2, 3], [1, 3, 5], [2, 6], [1, 5, 7], [2, 4, 6, 8], [3, 5, 9], [4, 8], [5, 7, 9], [6, 8]]
+visited = [False] * 10
+dfs(graph, 1, visited)
